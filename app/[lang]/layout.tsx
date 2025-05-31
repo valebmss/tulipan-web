ea5521788440754fe706a3168b4650424a2e37a9
valebmss <leidy.larrea01@usa.edu.co>
2025-05-31 13:06:30 -0500

    test

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,29 +1,30 @@
+import { ReactNode } from "react";
 import "../globals.css";
+import { getDictionary } from "@/lib/dictionary";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 import type { Metadata } from "next";
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
-import { getDictionary } from '@/lib/dictionary';
 
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "Tulipan 1637",
+  };
+}
 
-export const metadata: Metadata = {
-  title: "Tulipán",
-  description: "Resolvemos los problemas más difíciles con matemáticas.",
-};
-
-export default async function RootLayout({ children, params }: { children: React.ReactNode; params: { lang: 'es' | 'en' } }) {
-  const dict = await getDictionary(params.lang);
+export default async function Layout({
+  children,
+  params,
+}: {
+  children: ReactNode;
+  params: { lang: string }; // usa string y haces cast luego
+}) {
+  const lang = params.lang as "es" | "en";
+  const dict = await getDictionary(lang);
 
   return (
-    <html lang={params.lang}>
-      <body className="bg-black text-white antialiased font-sans">
-<Header
-  dict={{
-    nav_quehacemos: dict.nav_quehacemos,
-    nav_casos: dict.nav_casos,
-    nav_equipo: dict.nav_equipo,
-    nav_contacto: dict.nav_contacto
-  }}
-/>
+    <html lang={lang}>
+      <body>
+        <Header dict={dict} />
         {children}
         <Footer dict={dict} />
       </body>
